42e1c65bc57205306ce7c4a4f35747861fa6bf52
Talha Ibn Hafiz <talhaibnhafiz1@gmail.com>
2026-02-19 22:36:04 +0600

    Update person details in content.tsx

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -2,10 +2,10 @@ import { About, Blog, Gallery, Home, Newsletter, Person, Social, Work } from "@/
 import { Line, Row, Text } from "@once-ui-system/core";
 
 const person: Person = {
-  firstName: "Selene",
-  lastName: "Yu",
-  name: `Selene Yu`,
-  role: "Design Engineer",
+  firstName: "Md.Talha Ibn",
+  lastName: "Hafiz",
+  name: `Talha Ibn Hafiz`,
+  role: "CS Enthusiast",
   avatar: "/images/avatar.jpg",
   email: "example@gmail.com",
   location: "Asia/Jakarta", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'

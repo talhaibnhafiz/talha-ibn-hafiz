1a3afb90a347e855e2505e1181271f7070d70623
Talha Ibn Hafiz <talhaibnhafiz1@gmail.com>
2026-02-20 12:09:38 +0600

    Refactor technical skills and certifications sections

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -291,62 +291,62 @@ export default function About() {
             </>
           )}
 
-          {about.technical.display && (
-            <>
-              <Heading
-                as="h2"
-                id={about.technical.title}
-                variant="display-strong-s"
-                marginBottom="40"
-              >
-               {about.technical.title}
-</Heading>
-<Column fillWidth gap="l">
-  {about.technical.skills.map((skill, index) => (
-    <Column key={`${skill}-${index}`} fillWidth gap="4">
-      <Text id={skill.title} variant="heading-strong-l">
-        {skill.title}
-      </Text>
-      <Text variant="body-default-m" onBackground="neutral-weak">
-        {skill.description}
-      </Text>
-      {skill.tags && skill.tags.length > 0 && (
-        <Row wrap gap="8" paddingTop="8">
-          {skill.tags.map((tag, tagIndex) => (
-            <Tag key={`${skill.title}-${tagIndex}`} size="l" prefixIcon={tag.icon}>
-              {tag.name}
-            </Tag>
-          ))}
-        </Row>
-      )}
-      {skill.images && skill.images.length > 0 && (
-        <Row fillWidth paddingTop="m" gap="12" wrap>
-          {skill.images.map((image, index) => (
-            <Row
-              key={index}
-              border="neutral-medium"
-              radius="m"
-              minWidth={image.width}
-              height={image.height}
-            >
-              <Media
-                enlarge
-                radius="m"
-                sizes={image.width.toString()}
-                alt={image.alt}
-                src={image.src}
-              />
+         {about.technical.display && (
+  <>
+    <Heading
+      as="h2"
+      id={about.technical.title}
+      variant="display-strong-s"
+      marginBottom="40"
+    >
+      {about.technical.title}
+    </Heading>
+    <Column fillWidth gap="l">
+      {about.technical.skills.map((skill, index) => (
+        <Column key={`technical-${index}`} fillWidth gap="4">
+          <Text id={skill.title} variant="heading-strong-l">
+            {skill.title}
+          </Text>
+          <Text variant="body-default-m" onBackground="neutral-weak">
+            {skill.description}
+          </Text>
+          {skill.tags && skill.tags.length > 0 && (
+            <Row wrap gap="8" paddingTop="8">
+              {skill.tags.map((tag, tagIndex) => (
+                <Tag key={`tag-${index}-${tagIndex}`} size="l" prefixIcon={tag.icon}>
+                  {tag.name}
+                </Tag>
+              ))}
             </Row>
-          ))}
-        </Row>
-      )}
+          )}
+          {skill.images && skill.images.length > 0 && (
+            <Row fillWidth paddingTop="m" gap="12" wrap>
+              {skill.images.map((image, imgIndex) => (
+                <Row
+                  key={`img-${index}-${imgIndex}`}
+                  border="neutral-medium"
+                  radius="m"
+                  minWidth={image.width}
+                  height={image.height}
+                >
+                  <Media
+                    enlarge
+                    radius="m"
+                    sizes={image.width.toString()}
+                    alt={image.alt}
+                    src={image.src}
+                  />
+                </Row>
+              ))}
+            </Row>
+          )}
+        </Column>
+      ))}
     </Column>
-  ))}
-</Column>
-</>
+  </>
 )}
 
-{/* ------------------ ADD CERTIFICATIONS SECTION HERE ------------------ */}
+{/* ------------------ CERTIFICATIONS ------------------ */}
 {about.certifications?.display && (
   <>
     <Heading
@@ -359,7 +359,7 @@ export default function About() {
     </Heading>
     <Column fillWidth gap="l" marginBottom="40">
       {about.certifications.items.map((cert, index) => (
-        <Column key={`${cert.title}-${index}`} fillWidth gap="4">
+        <Column key={`cert-${index}`} fillWidth gap="4">
           <Row fillWidth horizontal="between">
             <Text id={cert.title} variant="heading-strong-l">
               {cert.title}
@@ -382,7 +382,7 @@ export default function About() {
   </>
 )}
 
-{/* ------------------ ADD AWARDS SECTION HERE ------------------ */}
+{/* ------------------ AWARDS ------------------ */}
 {about.awards?.display && (
   <>
     <Heading
@@ -395,7 +395,7 @@ export default function About() {
     </Heading>
     <Column fillWidth gap="l" marginBottom="40">
       {about.awards.items.map((award, index) => (
-        <Column key={`${award.title}-${index}`} fillWidth gap="4">
+        <Column key={`award-${index}`} fillWidth gap="4">
           <Row fillWidth horizontal="between">
             <Text id={award.title} variant="heading-strong-l">
               {award.title}

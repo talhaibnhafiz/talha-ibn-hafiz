a32d9154b235919377b99963fdff669526b148a0
Talha Ibn Hafiz <talhaibnhafiz1@gmail.com>
2026-02-22 22:31:46 +0600

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -25,7 +25,10 @@ export const Footer = () => {
           <Text onBackground="neutral-weak">© {currentYear} /</Text>
           <Text paddingX="4">{person.name}</Text>
           <Text onBackground="neutral-weak">
-           
+            {/* Usage of this template requires attribution. Please don't remove the link to Once UI unless you have a Pro license. */}
+            / {" "}
+            <SmartLink href="https://once-ui.com/products/magic-portfolio">Once UI</SmartLink>
+          </Text>
         </Text>
         <Row gap="16">
           {social.map(
